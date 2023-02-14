b24e01ee6af1786fc9947ad0d2d3adc51c392542
nurzhan <Salikh835@gmail.com>
2023-02-14 19:02:20 +0600

    done

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        int first = 1000;
+        int second = first;
+        System.out.println(first+" "+second);
     }
 }
